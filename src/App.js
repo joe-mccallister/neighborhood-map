5e1eafb77d016c9cfd43c145d5e481ae9c1bda81
Joe McCallister <macattack1380@gmail.com>
2018-10-30 13:19:23 -0600

    map display imparted, work on display

--- src/App.js
+++ src/App.js
@@ -1,14 +1,23 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
-   initMap = () => {
-     var map = new google.maps.Map(document.getElementById('map'), {
+
+  componentDidMount(){
+    this.renderMap()
+  }
+
+  renderMap = () => {
+    loadScript("https://maps.googleapis.com/maps/api/js?key=AIzaSyBVuYwtERyg82CFa4CH_NmIOlPBZBMxsfs&callback=initMap")
+    window.initMap = this.initMap
+  }
+
+  initMap = () => {
+     var map = new window.google.maps.Map(document.getElementById('map'), {
        center: {lat: -34.397, lng: 150.644},
-        zoom: 8
-        });
-      }
+       zoom: 8
+          })
+        }
 
   render() {
     return (
@@ -19,15 +28,8 @@ class App extends Component {
   }
 }
 
-/*
-<script src="https://maps.googleapis.com/maps/api/js?key=AIzaSyCjN9nt2zjeyRCPu6ueQAonZmDxePsBQNo&callback=initMap"
-    async defer></script>
-
-*/
-
 function loadScript(url){
-    var index = window.document.getElementsByTagName("script")
-    [0]
+    var index = window.document.getElementsByTagName("script")[0]
     var script = window.document.createElement("script")
     script.src = url
     script.async = true
